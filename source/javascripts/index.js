3b06a6c2ad93044b675b0627c6ad08734e52bf28
dylandy.chang <dylandy.chang@gmail.com>
2016-08-02 12:07:22 +0800

    basic index modification

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,9 +1,24 @@
 var place_section = function(){
   $("section").map(function(){
-    $(this).css("top",$(window).height()*$(this).index()+"px")
-  })  
+    $(this).css("top",$(window).height()*($(this).index()-1)+"px")
+  });  
+  $("footer").css("top", $("section").size() * $("section").height() + "px");
+  $("nav").css("top", ($(window).height() - $("nav").height()) + "px");
 }
+
+var fix_nav = function(){
+}
+
 
 jQuery(function($){
   place_section();
+  $(window).scroll(function(){
+    if($(document).scrollTop() < ($(window).height() - $("nav").height())){
+      $("nav").css("top", ($(window).height() - $("nav").height()) + "px");
+      $("nav").css("position", "absolute");
+    }else if($(document).scrollTop() >= ($(window).height() - $("nav").height())){
+      $("nav").css("position", "fixed");
+      $("nav").css("top", 0)
+    }  
+  })
 });
